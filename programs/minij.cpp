19d1132242c66b1d6730f7b8562cbe595a825f1f
Joshua Huelsman <joshuahuelsman@gmail.com>
2020-08-26 22:03:36 -0400

    minij: simple function calls now work. Add minij test file tests/simple.j.

--- programs/minij.cpp
+++ programs/minij.cpp
@@ -35,6 +35,7 @@ struct Token {
         IDENTIFIER,
         INTEGER,
         FLOAT,
+        STRING,
 
         KEYWORD_START,
         KEYWORD_FUNC = KEYWORD_START,
@@ -110,6 +111,40 @@ struct Lexer {
             
             buffer = end;
             return t;
+        } else if (*buffer == '"') {
+            buffer += 1;
+            char *end = buffer;
+            while (*end && *end != '"') end += 1;
+
+            Token t = token(Token::STRING);
+
+            auto length = end - buffer;
+            char *value = (char *)malloc(length + 1); // @Leak
+            t.string_value = value;
+
+            for (char *start = buffer; start < end; start += 1) {
+                if (*start == '\\') {
+                    start += 1;
+
+                    switch (*start) {
+                        case 'n':
+                            *value = '\n';
+                            break;
+
+                        default:
+                            *value = *start;
+                            break;
+                    }
+                } else {
+                    *value = *start;
+                }
+
+                value += 1;
+            }
+            *value = 0;
+
+            buffer = end+1;
+            return t;
         }
 
         Token t = token(*buffer);
@@ -127,8 +162,40 @@ struct Lexer {
 };
 
 namespace AST {
-    struct Expression {
+    enum {
+        INVALID,
+        FUNCTION_CALL,
+        LITERAL,
+    };
 
+    struct Expression {
+        u32 type;
+    };
+
+    struct Literal : Expression {
+        Literal() { type = LITERAL; }
+
+        enum {
+            STRING,
+            INTEGER,
+            FLOAT,
+        };
+
+        u32 literal_type;
+
+        union {
+            char * string_value;
+            u64    integer_value;
+            double float_value;
+        };
+
+    };
+
+    struct Function_Call : Expression {
+        Function_Call() { type = FUNCTION_CALL; }
+
+        char *call_target_name;
+        std::vector<Expression *> arguments;
     };
 
     struct Scope {
@@ -170,10 +237,57 @@ struct Parser {
         return true;
     }
 
+    AST::Function_Call *parse_function_call() {
+        if (!expect(Token::IDENTIFIER)) return nullptr;
+
+        Token name = peek_token();
+        next_token();
+
+        AST::Function_Call *call = new AST::Function_Call();
+        call->call_target_name = name.string_value;
+
+        if (!expect_and_eat('(')) return nullptr;
+
+        while (peek_token().type != ')') {
+            call->arguments.push_back(parse_expression());
+            if (peek_token().type == ',') next_token();
+        }
+
+        if (!expect_and_eat(')')) return nullptr;
+
+        return call;
+    }
+
+    AST::Expression *parse_expression() {
+        if (peek_token().type == Token::IDENTIFIER) {
+            return parse_function_call();
+        }
+
+        if (peek_token().type == Token::STRING) {
+            AST::Literal *lit = new AST::Literal();
+
+            Token t = peek_token();
+            lit->literal_type = AST::Literal::STRING;
+            lit->string_value = t.string_value;
+
+            next_token();
+            return lit;
+        }
+
+        return nullptr;
+    }
+
     AST::Scope *parse_scope() {
         if (!expect_and_eat('{')) return nullptr;
 
         AST::Scope *scope = new AST::Scope();
+
+        while (peek_token().type != '}') {
+            AST::Expression *expr = parse_expression();
+            if (expr) scope->statements.push_back(expr);
+
+            if (peek_token().type == ';') next_token();
+        }
 
         if (!expect_and_eat('}')) return nullptr;
 
@@ -203,6 +317,43 @@ struct Parser {
     }
 };
 
+Value *emit_expression(AST::Expression *expr, Compilation_Unit *unit, IR_Manager *irm) {
+    switch (expr->type) {
+        case AST::LITERAL: {
+            auto lit = static_cast<AST::Literal *>(expr);
+
+            switch (lit->literal_type) {
+                case AST::Literal::STRING : return make_string_constant(lit->string_value);
+                case AST::Literal::INTEGER: return make_integer_constant(lit->integer_value);
+            }
+            assert(false);
+            return nullptr;
+        }
+
+        case AST::FUNCTION_CALL: {
+            auto call = static_cast<AST::Function_Call *>(expr);
+
+            Array<Value *> args;
+
+            for (auto a : call->arguments) {
+                args.add(emit_expression(a, unit, irm));
+            }
+
+            Function *target = nullptr;
+            for (auto func : unit->functions) {
+                if (strcmp(call->call_target_name, func->name.data) == 0) {
+                    target = func;
+                    break;
+                }
+            }
+
+            assert(target);
+
+            return irm->insert_call(target, args);
+        }
+    }
+}
+
 int main(int argc, char **argv) {
     if (argc < 2) {
         printf("Usage: %s <source-file>\n", argv[0]);
@@ -221,21 +372,29 @@ int main(int argc, char **argv) {
     Parser parser;
     lex.tokenize_text(&parser.tokens);
 
+    /*
     for (auto t : parser.tokens) {
         if (t.type < Token::END) printf("Token: %c\n", t.type);
         else if (t.type == Token::IDENTIFIER) printf("Identifier: %s\n", t.string_value);
         else if (t.type >= Token::KEYWORD_START && t.type <= Token::KEYWORD_END) printf("Keyword: %s\n", keywords[t.type - Token::KEYWORD_START]);
+        else if (t.type == Token::STRING) printf("String: \"%s\"\n", t.string_value);
+    }
+    */
+
+    std::vector<AST::Function *> functions;
+    while (parser.peek_token().type == Token::KEYWORD_FUNC) {
+        AST::Function *function = parser.parse_function();
+        functions.push_back(function);
     }
 
     Compilation_Unit unit;
     unit.target = get_host_target();
 
-    while (parser.peek_token().type == Token::KEYWORD_FUNC) {
-        AST::Function *function = parser.parse_function();
-
+    for (auto function : functions) {
         Function *func = new Function();
         func->name = function->name;
         func->value_type = make_func_type(make_void_type());
+        unit.functions.add(func);
 
         if (function->body) {
             Basic_Block *block = new Basic_Block();
@@ -244,13 +403,16 @@ int main(int argc, char **argv) {
             IR_Manager *irm = new IR_Manager();
             irm->set_block(block);
 
+            for (auto expr: function->body->statements) {
+                emit_expression(expr, &unit, irm);
+            }
+
             irm->insert_return();
         }
-
-        unit.functions.add(func);
     }
 
-    emit_obj_file(&unit);
+    // emit_obj_file(&unit);
+    do_jit_and_run_program_main(&unit);
 
     return 0;
 }
---
+++ tests/simple.j
@@ -0,0 +1,10 @@
+
+func printf();
+
+func test() {
+    printf("hello world!\n");
+}
+
+func main() {
+    test();
+}
